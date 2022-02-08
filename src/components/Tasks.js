8184ee796e9ccb875671d5cca4de4cbd29b6b74c
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2022-02-08 23:17:38 +0100

    move tasks object to state
    we want the tasks object to be within the Task component and not outside the Task component

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,25 +1,27 @@
-const tasks = [
-    {
-        id: 1,
-        text: 'Take out trash',
-        day: 'Feb 5th at 5:30pm',
-        reminder: true
-    },
-    {
-        id: 2,
-        text: 'Meeting with Boss',
-        day: 'Feb 5th at 10:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false
-    }
-];
+import { useState } from "react";
+
 
 const Tasks = () => {
+    const [tasks, setTasks] = useState([
+        {
+            id: 1,
+            text: 'Take out trash',
+            day: 'Feb 5th at 5:30pm',
+            reminder: true
+        },
+        {
+            id: 2,
+            text: 'Meeting with Boss',
+            day: 'Feb 5th at 10:30pm',
+            reminder: true
+        },
+        {
+            id: 3,
+            text: 'Food Shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: false
+        }
+    ])
     return (
         <>
             {tasks.map((task)=>(<h3 key={task.id}>{task.text}</h3>))}
