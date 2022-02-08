f8ff837e04d76fcdb97206329de55e4dc502f45f
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2022-02-08 20:31:36 +0100

    update header

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="container">
+    <header className="container">
       <Header/>
-    </div>
+    </header>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 
-const Header = ({title}) => {
-  return <div>
-      <h1>{title}</h1>
-  </div>;
+const Header = ({ title }) => {
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <button className='btn'>Add</button>
+        </header>
+    )
 };
 
-Header.defaultProps = { 
+Header.defaultProps = {
     title: 'Task Tracker',
 }
 
