5aa4e68e3fd4fdbb5559d681f30bbbf677d1c6fc
Ibraheem-A <43247583+Ibraheem-A@users.noreply.github.com>
2022-02-13 22:34:55 +0100

    display form to add new task
    add form (AddTask.js) component to App.js

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -2,18 +2,18 @@ import React from 'react';
 
 const AddTask = () => {
     return (
-        <form className=' add-form'>
+        <form className='add-form'>
             <div className='form-control'>
                 <label>Task</label>
                 <input type='text' placeholder='Add Task'></input>
             </div>
-            <div className='form-control form-control-check'>
+            <div className='form-control'>
                 <label>Day & Time</label>
                 <input type='text' placeholder='Add Day & Time'></input>
             </div>
-            <div className='form-control'>
+            <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
-                <input type='checkbox'></input>
+                <input type='checkbox' />
             </div>
 
             <input type='submit' value='Save Task' className='btn btn-block'></input>
